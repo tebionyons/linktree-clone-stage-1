6bc256a66fd118581fc67ea3e4713fe70603678d
Tebi Remion <tembeiremion@gmail.com>
2022-10-22 17:21:52 +0100

    Fix bug preventing profile image from rendring

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+
+import React from "react"
+
+export default function Header(){
+    return(
+        <header className="header">
+           <img src="./images/tebi.jpg" id="profile__img" alt="profile image"/>
+           <h3 id="twitter">tebionyons</h3>
+        </header>
+    )
+}
