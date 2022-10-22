ce5bfa078c8a2da2f71dba6f4f7dfbc62e7a0828
Tebi Remion <tembeiremion@gmail.com>
2022-10-22 18:00:55 +0100

    Fix all the links and aligned to the center

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './Components/Header';
+import Links from './Components/Links';
 import logo from './logo.svg';
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div >
      <Header/>
+     <Links />
       
     </div>
   );
---
+++ src/Components/Links.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+
+export default function Links(){
+    return (
+        <div className="links-container">
+            <a id="btn-twitter" href="https://twitter.com/tebionyons">Twitter Link</a>
+            <a id="btn-zuri" href="https://training.zuri.team/">Zuri Team</a>
+            <a id="books" href="http://books.zuri.team ">Zuri Books</a>
+            <a id="book__python" href="https://books.zuri.team">Python Books</a>
+            <a id="pitch" href="https://background.zuri.team">Background Check for Coders</a>
+            <a id="book__design" href="https://books.zuri.team/design-rules ">Design Books</a>
+        </div>
+    )
+}
