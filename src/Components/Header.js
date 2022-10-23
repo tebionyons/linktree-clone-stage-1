0e72fbdde4628c6f1ec8025d2d162f82e8e4ccd0
Tebi Remion <tembeiremion@gmail.com>
2022-10-23 13:11:10 +0100

    Refactor and scale page for mobile version

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,9 +3,13 @@ import React from "react"
 
 export default function Header(){
     return(
+        <div>
+           
         <header className="header">
+        <img id="header-icon" src="../images/Icon.png"/>
            <img src="./images/tebi.jpg" id="profile__img" alt="profile image"/>
            <h3 id="twitter">tebionyons</h3>
         </header>
+        </div>
     )
 }
--- src/Components/Links.js
+++ src/Components/Links.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Links(){
     return (
         <div className="links-container">
-            <a id="btn-twitter" href="https://twitter.com/tebionyons">Twitter Link</a>
+            <a className="btn-twitter" id="btn-twitter" href="https://twitter.com/tebionyons">Twitter Link</a>
             <a id="btn-zuri" href="https://training.zuri.team/">Zuri Team</a>
             <a id="books" href="http://books.zuri.team ">Zuri Books</a>
             <a id="book__python" href="https://books.zuri.team">Python Books</a>
