d29d3c55be1a056c7b7b170022e28192a78b412f
Tebi Remion <tembeiremion@gmail.com>
2022-10-24 01:01:44 +0100

    Fix mobile  version of page and make responsive

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,10 +4,11 @@ import React from "react"
 export default function Header(){
     return(
         <div>
-           
+          
         <header className="header">
+        <img id="smallheader-icon" src="../images/avatar.png"/>
         <img id="header-icon" src="../images/Icon.png"/>
-           <img src="./images/tebi.jpg" id="profile__img" alt="profile image"/>
+           <img src="../images/tebi.jpg" id="profile__img" alt="profile image"/>
            <h3 id="twitter">tebionyons</h3>
         </header>
         </div>
